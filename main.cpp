6b19820842de26b409c068415aafab031683663b
EkaterinaMiller <evmiller@nic.edu>
2024-10-24 16:28:51 -0700

    say

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
-
+void say (string s);
 int main()
 {
     cout<<"Greeting\n";
     cout<<"Hello world\n";
+    say("hi");
     return 0;
+}
+void say (string s)
+{
+    cout<<s<<endl;
 }
